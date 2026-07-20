e70c098cb05866a62ece490ad8e3d7178807f371
نيار عبدالله <niyar.abdullah.1992@gmail.com>
2026-07-20 10:33:20 +0000

    File changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  assetsInclude: ["**/*.html?raw="],
+  assetsInclude: ["**/*.html?raw=", "**/*.css?raw="],
   plugins: [
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
