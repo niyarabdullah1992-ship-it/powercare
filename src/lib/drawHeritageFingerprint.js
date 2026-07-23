65eee0ccbe72da86cb981fd6bc580688168fb1f8
نيار عبدالله <niyar.abdullah.1992@gmail.com>
2026-07-23 15:42:06 +0000

    File changes

--- src/lib/drawHeritageFingerprint.js
+++ src/lib/drawHeritageFingerprint.js
@@ -1,43 +1,48 @@
 export default function drawHeritageFingerprint(ctx, cx, cy, size) {
-  const gold = "#d4af55";
-  const glow = "#f1d47a";
+  const track = "#071a2d";
+  const amber = "#e0a43b";
+  const glow = "#f2bd55";
+  const paths = [
+    [[-.48,-.27],[-.35,-.49],[0,-.56],[.35,-.49],[.48,-.27]],
+    [[-.53,.02],[-.49,-.31],[-.26,-.5],[0,-.53],[.29,-.45],[.47,-.18],[.51,.12]],
+    [[-.5,.29],[-.37,.14],[-.36,-.2],[-.18,-.41],[.08,-.46],[.33,-.29],[.41,.02],[.43,.3]],
+    [[-.42,.43],[-.24,.27],[-.27,-.08],[-.13,-.33],[.11,-.36],[.29,-.16],[.3,.18],[.4,.42]],
+    [[-.29,.5],[-.1,.31],[-.17,-.02],[-.06,-.25],[.13,-.26],[.22,-.06],[.16,.25],[.29,.51]],
+    [[-.1,.54],[.05,.34],[-.04,.05],[.02,-.14],[.13,-.12],[.14,.08],[.04,.32],[.12,.53]],
+    [[-.47,.12],[-.5,.32],[-.42,.48]],
+    [[.48,.13],[.49,.34],[.42,.49]],
+    [[-.34,.54],[-.18,.42],[-.08,.52]],
+    [[.22,.51],[.35,.57],[.43,.48]],
+  ];
+
+  const trace = (path, color, width, dash = []) => {
+    const points = path.map(([x, y]) => [cx + x * size, cy + y * size]);
+    ctx.beginPath();
+    ctx.moveTo(points[0][0], points[0][1]);
+    for (let index = 1; index < points.length - 1; index += 1) {
+      const [x, y] = points[index];
+      const [nextX, nextY] = points[index + 1];
+      ctx.quadraticCurveTo(x, y, (x + nextX) / 2, (y + nextY) / 2);
+    }
+    const last = points[points.length - 1];
+    ctx.lineTo(last[0], last[1]);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.setLineDash(dash);
+    ctx.stroke();
+  };
+
   ctx.save();
   ctx.lineCap = "round";
   ctx.lineJoin = "round";
-
-  const paths = [
-    [[-.42,-.22],[-.3,-.44],[0,-.5],[.3,-.44],[.42,-.22]],
-    [[-.48,-.04],[-.43,-.3],[-.24,-.48],[0,-.55],[.24,-.48],[.43,-.3],[.48,-.04]],
-    [[-.5,.15],[-.46,-.15],[-.31,-.39],[-.08,-.5],[.15,-.47],[.36,-.29],[.43,.02]],
-    [[-.48,.3],[-.36,.18],[-.34,-.13],[-.18,-.36],[.06,-.42],[.28,-.28],[.34,.04],[.4,.27]],
-    [[-.38,.41],[-.22,.28],[-.24,-.08],[-.12,-.29],[.08,-.34],[.24,-.18],[.22,.16],[.34,.4]],
-    [[-.25,.48],[-.1,.31],[-.14,-.05],[-.05,-.23],[.1,-.25],[.17,-.1],[.12,.19],[.25,.5]],
-    [[-.08,.5],[.02,.34],[-.04,.02],[.02,-.14],[.1,-.14],[.13,-.02],[.03,.24],[.1,.48]],
-    [[-.43,.1],[-.48,.28],[-.42,.42]],
-    [[.43,.12],[.46,.31],[.41,.44]],
-  ];
-
-  const stroke = (color, width) => {
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
-    paths.forEach((points) => {
-      ctx.beginPath();
-      const scaled = points.map(([x, y]) => [cx + x * size, cy + y * size]);
-      ctx.moveTo(scaled[0][0], scaled[0][1]);
-      for (let index = 1; index < scaled.length - 1; index += 1) {
-        const [x, y] = scaled[index];
-        const [nextX, nextY] = scaled[index + 1];
-        ctx.quadraticCurveTo(x, y, (x + nextX) / 2, (y + nextY) / 2);
-      }
-      const last = scaled[scaled.length - 1];
-      ctx.lineTo(last[0], last[1]);
-      ctx.stroke();
-    });
-  };
-
-  ctx.globalAlpha = .28;
-  stroke(glow, size * .075);
-  ctx.globalAlpha = 1;
-  stroke(gold, size * .045);
+  ctx.shadowColor = amber;
+  ctx.shadowBlur = size * .06;
+  paths.forEach((path) => trace(path, track, size * .095));
+  ctx.shadowBlur = 0;
+  paths.forEach((path, index) => {
+    const dash = index % 2 ? [0, size * .085] : [size * .11, size * .07];
+    trace(path, index % 3 === 1 ? glow : amber, size * .025, dash);
+  });
+  ctx.setLineDash([]);
   ctx.restore();
 }
